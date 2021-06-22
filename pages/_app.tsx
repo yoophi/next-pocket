338c62ac02a844e9b853b4fc79831f2fdd18f202
Pyunghyuk Yoo <yoophi@gmail.com>
2021-06-22 23:23:20 +0900

    ♻️ UPDATE: _app.js -> _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { wrapper } from "../app/store";
+import type { AppContext, AppProps } from 'next/app'
 import AuthProvider from "../shared/contexts/Auth";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <AuthProvider>
